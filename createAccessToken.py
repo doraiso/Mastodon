b1c723368395726e42aea1293cdd2734060e9305
doraiso <doraiso.dora@gmail.com>
2018-08-25 10:19:02 +0900

    Add comments

--- createAccessToken.py
+++ createAccessToken.py
@@ -1,15 +1,26 @@
+# https://takulog.info/exercise-python-for-mastodon-1-answer/
+# 4. インスタンスからアクセストークンを取得して、ファイル「access_token.txt」に保存してください。
+# 今回はパスワード方式で登録します。実運用では推奨されていません。
+# また、前問1-3で取得したClient IDおよびClient Secretを保存したファイルを利用します。
+# 同じく、以降の処理は全てここでのファイルを使うので、非常に重要です。
+# なお、EMAIL@EXAMPLE.COM および PASSWORD もアカウントの正確なものと読み替えてください。
+# ※この方法が褒められたものでは無いとは思いますが。。。
+
+
 from mastodon import Mastodon
 import json
 
-f = open("Mastodon/config.json",'r')
-json_data = json.load(f) #JSON形式で読み込む
+f = open("Mastodon/config.json",'r') # ここにメールアドレスとかパスワードぶっこんでる
+json_data = json.load(f) # JSON形式で読み込む
 
-url = 'https://mstdn.jp'
-username = 'doraiso'
-email = json_data[username]['email']
-password = json_data[username]['password']
-cid_file = 'Mastodon/client_id.txt'
-token_file = 'Mastodon/access_token.txt'
+url = 'https://mstdn.jp' # インスタンス
+username = 'doraiso' # これぼく
+
+email = json_data[username]['email'] # JSONからメールアドレス読むの
+password = json_data[username]['password'] # JSONからパスワード読むの
+
+cid_file = 'Mastodon/client_id.txt' # クライアントIDというやつ
+token_file = 'Mastodon/access_token.txt' # ここにアクセストークンを入れるの
 
 mastodon = Mastodon(
     client_id=cid_file,
--- createClientId.py
+++ createClientId.py
@@ -1,6 +1,10 @@
+# 3. インスタンスへクライアント「Python_Exercise」を登録して、Client ID と Client Secret を取得してください。
+# Client ID と Client Secret は Mastodon.pyモジュールを使えば、1つの関数で1つのファイルに格納してくれます。
+# 以降の処理は全てこのファイルを使うので、非常に重要です。
+
 from mastodon import Mastodon
 
-url = 'https://mstdn.jp'
+url = 'https://mstdn.jp' # インスタンス
 appname = 'Python_Exercise'
 cid_file = 'Mastodon/client_id.txt'
 
