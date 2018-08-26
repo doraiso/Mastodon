0127da068edd0708e90a561522c36bdb8c66d952
doraiso <doraiso.dora@gmail.com>
2018-08-26 23:24:59 +0900

    create tootUho.py

---
+++ tootUho.py
@@ -0,0 +1,38 @@
+import random
+from mastodon import Mastodon
+
+url = 'https://mstdn.jp/'
+cid_file = 'client_id.txt'
+token_file = 'access_token.txt'
+
+mastodon = Mastodon(
+    client_id=cid_file,
+    access_token=token_file,
+    api_base_url=url
+)
+
+strinit = 'ンホ'
+strnho = ['ォ','ッ','！']
+repnum = 3
+
+def nho(strinput):
+    str = ''
+    rep = random.randint(1,repnum)
+    for num in range(rep):
+        str += strinput
+    return str
+
+def appendnho():
+    str = ''
+    for num in strnho:
+        str += nho(num)
+    return str
+
+nho = strinit + appendnho()
+
+print(nho)
+
+mastodon.status_post(
+    nho,
+    spoiler_text='ンホォ #Python練習'
+)
